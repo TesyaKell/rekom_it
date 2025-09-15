71ad11d5aa7e2a0f7fdac0c7973d245046d05f12
Imanuella Daniel <220711901@students.uajy.ac.id>
2025-09-15 14:13:58 +0700

    integrasi login

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::post('/login', [App\Http\Controllers\userController::class, 'login']);
+
+Route::get('/homee', function () {
+    return view('homee');
+});
