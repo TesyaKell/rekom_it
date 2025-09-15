8914151f5bacfd1fd7edb3ea3bb370243fb52c86
Imanuella Daniel <220711901@students.uajy.ac.id>
2025-09-15 14:27:19 +0700

    integrasi rek

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
-
 Route::post('/login', [App\Http\Controllers\userController::class, 'login']);
+Route::get('/home', function () {
+    return view('home');
+});
 
-Route::get('/homee', function () {
-    return view('homee');
+
+//REKOMENDASI
+Route::get('/daftar_rekomendasi', function () {
+    return view('daftar_rekomendasi');
+});
+Route::get('/add_rekomendasi', function () {
+    return view('add_rekomendasi');
 });
